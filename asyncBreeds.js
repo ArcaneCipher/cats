e912ad8f19003151024b9ee9ceea1b6e8b95ba33
ArcaneCipher <desjardins.jarp@gmail.com>
2024-10-21 00:43:37 -0400

    fixed asyncBreeds.js

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,37 +1,37 @@
 // asyncBreeds.js
-const fs = require("fs");
-
-const breedDetailsFromFile = function (breed) {
-  console.log("breedDetailsFromFile: Calling readFile...");
-
-  // This function triggers the reading of a file asynchronously.
-  // The fs.readFile function takes in the file path, encoding, and a callback.
-  // The callback will be executed once the file reading is completed.
-  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
+const fs = require('fs');
+// Failing code: 
+/* 
+const breedDetailsFromFile = function(breed) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-
-    // ISSUE: 
-    // Returning from *inner* callback function, not breedDetailsFromFile.
-    // This `if` block checks if there's no error and tries to return the `data` inside the callback.
-    // However, this return statement is inside the asynchronous callback.
-    // The breedDetailsFromFile function itself will return *before* this callback is executed because
-    // the file reading is asynchronous and takes time.
-    // The return value of the callback doesn't affect the outer function.
-    if (!error) return data;
+    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
     if (!error) return data;
   });
-  // ISSUE: 
-  // Attempting to return data out here will also not work.
-  // Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
-
-  // The function continues to execute after calling fs.readFile, even before the file reading finishes.
-  // By the time the function gets here, it hasn't waited for the callback to finish.
-  // So, no data is available yet, and this function doesn't return anything (undefined).
-  // Attempting to return data at this point doesn't work because the readFile's callback hasn't executed yet.
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
 // we try to get the return value
-// This will log 'undefined' because breedDetailsFromFile returned undefined as it didn't wait
-// for the fs.readFile callback to complete.
-const bombay = breedDetailsFromFile("Bombay");
-console.log("Return Value: ", bombay); // => will NOT print out details, instead we will see undefined!
+const bombay = breedDetailsFromFile('Bombay');
+console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+ */
+
+// Corrected code: 
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed) // => print out details correctly.
+};
+
+// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+breedDetailsFromFile('Bombay', printOutCatBreed);
